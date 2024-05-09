53c5c8ac7ff54caaa3424e584421fb2f5ce4588e
kaustubh wavare <kaustubha2000wavare@gmail.com>
2024-05-09 10:55:38 +0530

    add some code in 20,21 line at program.cs file

--- login/Program.cs
+++ login/Program.cs
@@ -17,6 +17,10 @@ namespace login
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Form2());
+            Application.Run(new Form3());
+            //change some code 
+
         }
     }
 }
